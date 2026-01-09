355e07bc7348d335f42cb2d8d449317896fa33e5
Jacque Taylor <jacqt4@gmail.com>
2026-01-09 12:15:45 -0800

    New HelloWorld
    simple console app

---
+++ Program.cs
@@ -0,0 +1,5 @@
+Console.WriteLine("Hello, World! I am your C# program.");
+Console.WriteLine("This is a simple C# application");
+Console.WriteLine("Written by JacqT.");
+Console.WriteLine("I hope you find it useful!");
+Console.WriteLine("Goodbye!");
---
+++ README.md
@@ -0,0 +1,21 @@
+# HelloWorld
+
+Simple .NET console app in C#.
+
+Build:
+
+```bash
+dotnet build
+```
+
+Run:
+
+```bash
+dotnet run --project HelloWorld.csproj
+```
+
+Or, when in the `HelloWorld` folder:
+
+```bash
+dotnet run
+```
